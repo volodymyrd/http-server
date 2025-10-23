c60619f449adc08b49aba2ea5db7dd999a7966f6
volodymyrd <volodymyrdotsenko@gmail.com>
2025-10-23 23:39:57 +0200

    add 404 some refactoring.

--- src/main.rs
+++ src/main.rs
@@ -10,21 +10,19 @@ fn main() {
     }
 
     fn handle_connection(mut stream: TcpStream) {
-        let buff_reader = BufReader::new(&stream);
-        let http_request: Vec<_> = buff_reader
-            .lines()
-            .map(|line| line.unwrap())
-            .take_while(|line| !line.is_empty())
-            .collect();
+        let buf_reader = BufReader::new(&stream);
+        let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-        println!("Request: {http_request:#?}");
+        let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+            ("HTTP/1.1 200 OK", "hello.html")
+        } else {
+            ("HTTP/1.1 404 NOT FOUND", "404.html")
+        };
 
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("hello.html").unwrap();
+        let contents = fs::read_to_string(filename).unwrap();
         let length = contents.len();
 
-        let response =
-            format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
         stream.write_all(response.as_bytes()).unwrap();
     }
