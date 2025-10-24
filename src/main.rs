ba8a780602acba2126a5ca8f1f08ebcd946cf716
volodymyrd <volodymyrdotsenko@gmail.com>
2025-10-24 23:38:01 +0200

    fix integration tests.

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,17 @@ mod server;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    async fn handle_request(request: HttpRequest) -> HttpResponse {
-        if request.path() == "GET / HTTP/1.1\r\n" {
-            HttpResponse::new("HTTP/1.1 200 OK", "hello.html")
-        } else {
-            HttpResponse::new("HTTP/1.1 404 NOT FOUND", "404.html")
-        }
-    }
-
     let listener = TcpListener::bind("127.0.0.1:7878")
         .await
         .map_err(Error::Io)?;
     Server::new(listener).run(handle_request).await?;
     Ok(())
+}
+
+async fn handle_request(request: HttpRequest) -> HttpResponse {
+    if request.path() == "GET / HTTP/1.1\r\n" {
+        HttpResponse::new("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        HttpResponse::new("HTTP/1.1 404 NOT FOUND", "404.html")
+    }
 }
